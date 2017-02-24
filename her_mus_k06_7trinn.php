0b3056079028202f120db4a7a65e151ac7fa250b
tommit1970 <epleregime@gmail.com>
2017-02-24 12:02:45 +0100

    1. bodystyle.css(refreshed thoroughly) 2. navstyle.css(minor adjustments) 3. section_k06_07trinn_her_mus.inc.php(copied into her_mus_k06_7trinn.php

--- her_mus_k06_7trinn.php
+++ her_mus_k06_7trinn.php
@@ -18,13 +18,234 @@
 	<main>
 
 
-		<section id="k06">
+		<section>
+			<div id="master">
+				<h2>Kunnskapsløftet (K-06)</h2>
+				<article id="k06_07_maal_alle">
 
-			<?php include 'included/section_k06_7trinn_her_mus.inc.php'; ?>
-		
+				<h3><u>M&aring;l etter 7.trinn</u></h3><h4 id="brukstips">Trykk på et listepunkt(link) eller la musepekeren sveve over et ord i kursiv(definisjon)</h4>
+
+
+					<h4>Musisere</h4>
+					M&aring;l for oppl&#230;ringen er at eleven skal kunne
+					<ul>
+						<li><!-- oppfatte og anvende puls, rytme, form, melodi, klang, dynamikk og enkel harmonikk i lytting og musisering -->
+							<button  id="k06_07_01">
+							<a href="#elevMaaltitle">
+							<dfn title=
+								"Å oppfatte er det samme som å forstå.">
+								oppfatte</dfn>
+								 og <dfn title="Å anvende betyr å bruke.">
+								 anvende</dfn>
+							 <dfn title=
+								 "Pulsen er grunnrytmen i musikken, som ofte er jevn, men den kan forandre seg, øke eller sakke.">
+								 puls</dfn>, 
+								 <dfn title=
+								"Rytmen følger pulsen. Den kan gå P&aring; og UTENFOR pulsens slag, men den følger alltid pulsen.">
+								rytme</dfn>, 
+								<dfn title=
+								"Form i musikk handler om Start og Slutt, Vers og Refreng, Intro - Outro - Mellomspill, Samspill og Solo.">
+								form</dfn>, 
+								<dfn title=
+								"Melodien er toner som kommer etter hverandre. Det er melodien vi kjenner igjen i en sang.">
+								melodi</dfn>, 
+								<dfn title=
+								"Klangen er lydens kjennetegn. Noen lyder har hard klang(f.eks.metall), mens andre lyder har myk klang(f.eks.tre). Klang handler om lydens kvalitet, egenskap. Rund/spiss, fyldig/spinkel, rik eller fattig klang. Klang kan også handle om etterklang, dvs. hvordan lyden henger igjen i rommet etterpå.">
+								klang</dfn>, 
+								<dfn title=
+								"Dynamikken handler om styrken på lyden. Sterk eller svak lyd. Piano(svakt) eller Forte(sterkt).">
+								dynamikk</dfn>, 
+								<dfn title=
+								"Tempoet er farten på pulsen. BPM(Beat Per Minute/norsk: Slag i minuttet).">
+								tempo</dfn>
+							 og enkel 
+							 <dfn title=
+								"Harmonikk ER om toner som spilles samtidig og rundtomkring melodien, kler melodien med mer eller mindre rytmiske TONER, leier melodien hit og dit, skaper spenning og oppløsning.">
+								harmonikk</dfn>
+							 i <dfn title=
+								"Lytting er å aktivt høre etter klang, melodi, puls, rytme, harmonikk, form, tempo og dynamikk.">
+								lytting</dfn>
+							 og <dfn title=
+								"Musisering er å gjøre musikk, dvs. å spille et instrument eller å synge.">
+								musisering</dfn>
+							</a>
+							</button>
+						</li>
+						<li><!-- synge unisont og flestemt i gruppe med vekt på intonasjon, klang og uttrykk -->
+						<button  id="k06_07_02">
+						<a href="#elevMaaltitle">
+							synge
+							<dfn title=
+								"Unisont er enstemmig og flerstemt er..ja..riktig...flerstemt.">
+							unisont og flerstemt</dfn>
+							i gruppe med vekt p&aring;
+							<dfn title=
+								"Intonasjon handler om å treffe &#34;TONEN&#34; når du synger eller spiller. En tone har en frekvens, dvs. svingninger i sekundet, vibrasjon i instrumentet eller i stemmebåndet. Dårlig intonasjon er når du ubevisst synger litt under(lavere) eller litt over(høyere) tonen, altså uten å høre forskjellen. Har du kontroll og gjør dette bevisst, har du god intonasjon.">
+							intonasjon</dfn>
+							, klang	og 
+							<dfn title=
+								"Uttrykket kan være voldsomt, følsomt, tøft, vakkert, stilig, osv.....">
+							uttrykk</dfn>
+							</a>
+							</button>
+						</li>
+						<li><!-- beherske enkelt melodispill etter gehør og enkle harmoniske og rytmiske akkompagnement -->
+						<button  id="k06_07_03">
+							<a href="#elevMaaltitle">
+							beherske 
+							<dfn title="Enkelt melodispill betyr melodier som er korte og rytmisk enkle med rolig tempo og enkel styrkevariasjon.">
+							enkelt melodispill</dfn>
+							etter 
+							<dfn title="Gehør er en indre musikalsk stemme. Den kan du bruke til å sammenligne med det du hører(f.eks.piano eller sang). Hvis du kjenner en melodi, har du på en måte melodien inni hodet ditt. Du kan prøve å få det du spiller eller synger til å høres likt ut, som det du hører inni hodet ditt.">
+							geh&oslash;r</dfn>
+							og 
+							<dfn title="Et arrangement er en ferdig oppsatt musikkEnkle harmoniske akkompagnement er få toner som spilles ved siden av melodien. som spilles ved siden av melodien.">
+							enkle harmoniske og rytmiske akkompagnement</dfn>
+							</a>
+						</button>
+						</li>
+						<li><!-- framføre sanger og viser fra eldre og nyere tid -->
+						<button  id="k06_07_04">
+							<a href="#elevMaaltitle">
+							framf&oslash;re sanger og viser fra 
+							<dfn title="Nyere tid må vel være etter 1970 og eldre tid før det.">
+							eldre og nyere tid</dfn>
+							</a>
+						</button>
+						</li>
+						<li><!-- beherske noen norske danser og danser fra andre land -->
+						<button  id="k06_07_05">
+							<a href="#elevMaaltitle">
+							beherske noen 
+							<dfn title="F.eks. Polka, Reinlender, Ril og Pols....">
+							norske danser</dfn>
+							og 
+							<dfn title="F.eks. Polonaise(Frankrike), Swing(USA), Linedance(USA), Troika(Russland), Jenka(Finnland) og Zorba(Hellas).">
+							danser fra andre land</dfn>
+							</a>
+						</button>
+						</li>
+						<li><!-- delta i framføring med sang, spill og dans der egenkomponert musikk og dans inngår --><!--  -->
+						<button  id="k06_07_06">
+							<a href="#elevMaaltitle">
+							delta i framf&oslash;ring med sang, spill og dans der 
+							<dfn title="Eleven(e) har laget en enkel komposisjon(musikk og/eller dans). Fokus på enkelhet i Dynamikk/Styrke, Form/Mønster, Harmonikk/Flere, Klang/Stemning, Melodi/Tema/Motiv, Puls/Grunnslag, Rytme og Tempo/Fart.">
+							egenkomponert</dfn>
+							musikk og dans inng&aring;r
+							</a>
+						</button>
+						</li>
+					</ul>
+					
+					<h4>Komponere</h4>
+					M&aring;l for oppl&oslash;ringen er at eleven skal kunne
+					<ul>
+						<li>
+						<button  id="k06_07_07">
+							<a href="#elevMaaltitle">
+							improvisere med stemme og instrumenter med utgangspunkt i enkle rytmiske, melodiske og harmoniske m&oslash;nstre
+							</a>
+						</button>			
+						</li>
+						
+						<li>
+						<button  id="k06_07_08">
+							<a href="#elevMaaltitle">			
+							uttrykke egne ideer, tanker og f&oslash;lelser gjennom bevegelser og dans
+							</a>
+						</button>			
+						</li>
+						<li>
+						<button  id="k06_07_09">
+							<a href="#elevMaaltitle">lage egne komposisjoner med utg.pkt. i enkle musikalske former og motiver og bruke grafisk notasjon til &aring; lage skisser av komposisjonene
+							</a>
+						</button>			
+						</li>
+						<li>
+						<button  id="k06_07_10">
+							<a href="#elevMaaltitle">			
+							komponere og gj&oslash;re lydopptak ved hjelp av digitale verkt&oslash;y
+							</a>
+						</button>			
+						</li>
+					</ul>
+					
+					<h4>Lytte</h4>
+					M&#229;l for oppl&#230;ringen er at eleven skal kunne
+					<ul>
+						<li>
+						<button  id="k06_07_11">
+							<a href="#elevMaaltitle">			
+							gjenkjenne musikk fra historiske hovedepoker i kunstmusikken
+							</a>
+						</button>			
+						</li>
+						<li>
+						<button  id="k06_07_12">
+							<a href="#elevMaaltitle">			
+							diskutere s&#230;rtrekk ved kunstmusikk, norsk og samisk folkemusikk, folkemusikk fra andre land og rytmisk musikk
+							</a>
+						</button>			
+						</li>
+						<li>
+						<button  id="k06_07_13">
+							<a href="#elevMaaltitle">			
+							gi uttrykk for opplevelser i m&#248;te med verker av sentrale komponister fra kunstmusikken
+							</a>
+						</button>			
+						</li>
+						<li>
+						<button  id="k06_07_14">
+							<a href="#elevMaaltitle">			
+							gjenkjenne klangen til og benevne de ulike instrumentgruppene
+							</a>
+						</button>			
+						</li>
+						<li>
+						<button  id="k06_07_15">
+							<a href="#elevMaaltitle">			
+							samtale om hvordan musikk b&#229;de er et kunstnerisk uttrykk og en kommersiell vare
+							</a>
+						</button>			
+						</li>
+						<li>
+						<button  id="k06_07_16">
+							<a href="#elevMaaltitle">			
+							gi uttrykk for egne vurderinger om hvordan musikkens bruk og funksjon har endret seg gjennom tidene
+							</a>
+						</button>			
+						</li>
+					</ul>		
+					
+				</article>
+
+			
+				<aside id="k06_07_maal_utvalgt">
+					<h4>Hva betyr å</h4>
+						<p><li id="randomGoal"></li></p>
+				</aside>
+			
+
+			<!--
+			<dfn title=""><a href="#"></a></dfn>
+
+			 -->
+			</div>
 		</section>
 		
+		<section>
+			<div id="sub">
+				<article>
+					<div id="k06_num">
+					</div><div id="k06_stikkord"></div>
+					<div id="k06_type"></div>
+				</article>
+				
+				<aside>
 
+				</aside>
+			</div>
+		</section>
 		
 
 		
